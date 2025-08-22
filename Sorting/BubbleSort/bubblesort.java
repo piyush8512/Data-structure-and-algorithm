ab0b8e49de30f6cce9c0e48a5229ac66762094fe
piyush8512 <prajapatipiyush851@gmail.com>
2025-08-22 09:06:52 +0530

    buble Sort

---
+++ Sorting/BubbleSort/bubblesort.java
@@ -0,0 +1,40 @@
+package Sorting.BubbleSort;
+
+public class bubblesort {
+    static void bubble_sort(int[] arr, int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            int didSwap = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    didSwap = 1;
+                }
+            }
+            if (didSwap == 0) {
+                break;
+            }
+        }
+
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        // int arr[] = { 13, 46, 24, 52, 20, 9 };
+        int arr[] = { 9, 13, 20, 24, 46, 52 };
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble_sort(arr, n);
+    }
+
+}
