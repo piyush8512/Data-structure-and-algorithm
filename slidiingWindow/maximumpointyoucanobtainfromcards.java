928dfad1355fe6e7d7fd9635886b63ce67f2a51b
piyush8512 <prajapatipiyush851@gmail.com>
2026-06-08 10:58:42 +0530

    maximumpointyoucanobtainfromcards, trappingRainwate

--- slidiingWindow/maximumpointyoucanobtainfromcards.java
+++ slidiingWindow/maximumpointyoucanobtainfromcards.java
@@ -1,32 +1,68 @@
-// // public class maximumpointyoucanobtainfromcards {
+// // // public class maximumpointyoucanobtainfromcards {
     
+// // // }
+
+
+// // //bruteforcesolution
+// // class Solution {
+// //     public int maxScore(int[] cardPoints, int k) {
+// //         int n = cardPoints.length;
+// //         int maxSum = 0;
+// //         for (int i = 0; i <= k; i++) {
+// //             int tempSum = 0;
+// //             for (int j = 0; j < i; j++) {
+// //                 tempSum += cardPoints[j];
+// //             }
+// //             for (int j = 0; j < k - i; j++) {
+// //                 tempSum += cardPoints[n - 1 - j];
+// //             }
+// //             maxSum = Math.max(maxSum, tempSum);
+// //         }
+// //         return maxSum;
+// //     }
+// // }
+
+// // public class maximumpointyoucanobtainfromcards {
+// //     public static void main(String[] args) {
+// //         Solution sol = new Solution();
+// //         int[] cards = {1, 2, 3, 4, 5, 6, 1};
+// //         int k = 3;
+// //         System.out.println(sol.maxScore(cards, k));
+// //     }
 // // }
 
 
-// //bruteforcesolution
+// //tc: O(k^2)
+// //sc: O(1)
+
+
+// //optmal solution
 // class Solution {
 //     public int maxScore(int[] cardPoints, int k) {
 //         int n = cardPoints.length;
-//         int maxSum = 0;
-//         for (int i = 0; i <= k; i++) {
-//             int tempSum = 0;
-//             for (int j = 0; j < i; j++) {
-//                 tempSum += cardPoints[j];
-//             }
-//             for (int j = 0; j < k - i; j++) {
-//                 tempSum += cardPoints[n - 1 - j];
-//             }
-//             maxSum = Math.max(maxSum, tempSum);
+//         int total = 0;
+//         for (int i = 0; i < k; i++) {
+//             total += cardPoints[i];
 //         }
-//         return maxSum;
+//         int maxPoints = total;
+//         for (int i = 0; i < k; i++) {
+//             total -= cardPoints[k - 1 - i];
+//             total += cardPoints[n - 1 - i];
+//             maxPoints = Math.max(maxPoints, total);
+//         }
+//         return maxPoints;
 //     }
 // }
-
+ 
 // public class maximumpointyoucanobtainfromcards {
 //     public static void main(String[] args) {
-//         Solution sol = new Solution();
 //         int[] cards = {1, 2, 3, 4, 5, 6, 1};
 //         int k = 3;
+//         Solution sol = new Solution();
 //         System.out.println(sol.maxScore(cards, k));
 //     }
 // }
+
+
+//tc: O(k)
+//sc: O(1)
--- stack/trappingRainwater.java
+++ stack/trappingRainwater.java
@@ -1,25 +1,68 @@
-// // public class trappingRainwater{}
+// // // public class trappingRainwater{}
 
 
-// //bruteforceslution
+// // //bruteforceslution
+// // class Solution {
+// //     public int trap(int[] height) {
+// //         int n = height.length;
+// //         int totalWater = 0;
+// //         for (int i = 0; i < n; i++) {
+// //             int maxLeft = 0;
+// //             int maxRight = 0;
+// //             for (int j = 0; j <= i; j++) {
+// //                 if (height[j] > maxLeft) {
+// //                     maxLeft = height[j];
+// //                 }
+// //             }
+// //             for (int j = i; j < n; j++) {
+// //                 if (height[j] > maxRight) {
+// //                     maxRight = height[j];
+// //                 }
+// //             }
+// //             totalWater += Math.min(maxLeft, maxRight) - height[i];
+// //         }
+// //         return totalWater;
+// //     }
+// // }
+
+// // public class trappingRainwater {
+// //     public static void main(String[] args) {
+// //         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+// //         Solution sol = new Solution();
+// //         int result = sol.trap(height);
+// //         System.out.println("Trapped Rainwater: " + result);
+// //     }
+// // }
+// // //tc: O(n^2)
+// // //sc: O(1)
+
+
+// //optimal solutiion
+
 // class Solution {
 //     public int trap(int[] height) {
 //         int n = height.length;
+//         int left = 0;
+//         int right = n - 1;
+//         int maxLeft = 0;
+//         int maxRight = 0;
 //         int totalWater = 0;
-//         for (int i = 0; i < n; i++) {
-//             int maxLeft = 0;
-//             int maxRight = 0;
-//             for (int j = 0; j <= i; j++) {
-//                 if (height[j] > maxLeft) {
-//                     maxLeft = height[j];
+//         while (left <= right) {
+//             if (height[left] <= height[right]) {
+//                 if (height[left] >= maxLeft) {
+//                     maxLeft = height[left];
+//                 } else {
+//                     totalWater += maxLeft - height[left];
 //                 }
-//             }
-//             for (int j = i; j < n; j++) {
-//                 if (height[j] > maxRight) {
-//                     maxRight = height[j];
+//                 left++;
+//             } else {
+//                 if (height[right] >= maxRight) {
+//                     maxRight = height[right];
+//                 } else {
+//                     totalWater += maxRight - height[right];
 //                 }
+//                 right--;
 //             }
-//             totalWater += Math.min(maxLeft, maxRight) - height[i];
 //         }
 //         return totalWater;
 //     }
@@ -33,10 +76,5 @@
 //         System.out.println("Trapped Rainwater: " + result);
 //     }
 // }
-// //tc: O(n^2)
-// //sc: O(1)
-
-
-//optimal solutiion
-
-
+//tc: O(n)
+//sc: O(1)
